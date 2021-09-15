618332eaf103067d6ac87de39c70a89378303b82
EddyBautista <bautista.j.eddy@gmail.com>
2021-09-15 06:36:16 -0500

    new question added

--- Program.cs
+++ Program.cs
@@ -202,6 +202,23 @@ namespace arrays
                 Console.WriteLine(bribe);
         }
 
+        // Minimum Swaps 2
+        // You are given an unordered array consisting of consecutive integers 
+        // [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any 
+        // two elements. Find the minimum number of swaps required to sort the array 
+        // in ascending order.
+
+        static int minimumSwaps(int[] arr)
+        {
+
+            return 0;
+
+        }
+
+
+
+
+
         private static void Main(string[] args)
         {
 
